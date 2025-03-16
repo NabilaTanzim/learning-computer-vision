e950b4ec1b3054d8368a1bf0a62cd4626465428f
NabilaTanzim <nabila.tahwa@gmail.com>
2025-03-17 00:02:38 +0600

    reading with OpenCV

---
+++ photos/read.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+#read images
+
+img = cv.imread('photos/cat-1.jpg')
+
+cv.imshow('Cats', img)
+
+cv.waitKey(0)
+
+#read videos
+
+capture = cv.VideoCapture(0)
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(200) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
+
---
+++ photos/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+# cv.waitKey(0)
+
+#read videos\
+
+capture = cv.VideoCapture(0)
+
+while True:
